a9d8de803a89f72e1810a701521c9313f2711871
KubaG1974 <kuba.gorzynski@gmail.com>
2023-07-29 19:40:30 +0200

    Added DTOs directory, AuthController.cs

--- BandBlender/Controllers/AuthController.cs
+++ BandBlender/Controllers/AuthController.cs
@@ -1,6 +1,35 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BandBlender.DTOs;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
+
+
 namespace BandBlender.Controllers;
 
-public class AuthController
+[Microsoft.AspNetCore.Components.Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    
+    [HttpPost]
+    public IActionResult Login(UserForAuthenticationDto userForAuth)
+    {
+        // here, you would usually authenticate the user credentials first
+        // if they are valid, generate and return a JWT
+
+        var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSuperSecureKey"));
+        var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenOptions = new JwtSecurityToken(
+            issuer: "http://localhost:5000",
+            audience: "http://localhost:5000",
+            claims: new List<Claim>(), // you could add some user-specific claims here
+            expires: DateTime.Now.AddMinutes(30), // token expiration time
+            signingCredentials: signinCredentials
+        );
+
+        var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        return Ok(new { Token = tokenString });
+    }
 }
---
+++ BandBlender/DTOs/UserForAuthenticationDto.cs
@@ -0,0 +1,7 @@
+namespace BandBlender.DTOs;
+
+public class UserForAuthenticationDto
+{
+    public string email { get; set; }
+    public string password { get; set; }
+}
