dec65e129ecd93c4f37dfc5ca1833cc883304608
KubaG1974 <kuba.gorzynski@gmail.com>
2023-09-14 11:17:05 +0200

    Updated MusicianService file

--- BandBlender/Services/MusicianService.cs
+++ BandBlender/Services/MusicianService.cs
@@ -1,6 +1,31 @@
+using BandBlender.Models.DTOs.Musician;
+
 namespace BandBlender.Services;
 
 public class MusicianService : IMusicianService
 {
-    
+    public Task<IEnumerable<MusicianReadDto>> GetAllMusicians()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MusicianReadDto> GetMusicianById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Guid> CreateMusician(MusicianCreateDto musicianCreateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateMusician(Guid id, MusicianUpdateDto musicianUpdateDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteMusician(Guid id)
+    {
+        throw new NotImplementedException();
+    }
 }
